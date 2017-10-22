83dd2b82ada31d8d3fb0e419ebe916d6758ce066
Matthew Crumley <email@matthewcrumley.com>
2017-10-22 18:11:46 -0400

    Fix autocomplete tests

--- spec/autocomplete-spec.js
+++ spec/autocomplete-spec.js
@@ -27,52 +27,181 @@
 
 "use babel";
 
-let suggestionsForPrefix = (provider, editor, prefix, options) => {
-  let bufferPosition = editor.getCursorBufferPosition()
-  let scopeDescriptor = editor.getLastCursor().getScopeDescriptor()
-  let suggestions = provider.getSuggestions({editor, bufferPosition, prefix, scopeDescriptor})
-  if (options && options.raw) {
-    return suggestions
-  } else {
-    if (suggestions) {
-      return (suggestions.map((sug) => sug.text))
-    } else {
-      return []
-    }
-  }
-}
-
 describe("todo.txt autocompletion", () => {
-  var provider;
+  var provider
 
   const workspaceElement = () => {
     return atom.views.getView(atom.workspace)
-  };
+  }
+
+  const getCurrentLine = (te) => {
+    let pt = te.getCursorBufferPosition()
+    return te.lineTextForBufferRow(pt.row)
+  }
+
+  const suggestionsForPrefix = (provider, editor, options) => {
+    let bufferPosition = editor.getCursorBufferPosition()
+    let suggestions = provider.getSuggestions({editor, bufferPosition, activatedManually: (options && options.activatedManually)})
+    if (options && options.raw) {
+      return suggestions
+    } else {
+      if (suggestions) {
+        return (suggestions.map((sug) => sug.text))
+      } else {
+        return []
+      }
+    }
+  }
 
   beforeEach(() => {
     waitsForPromise(() => {
-      return atom.workspace.open("todo.txt");
-    });
+      return atom.workspace.open("todo.txt")
+    })
 
     waitsForPromise(() => {
       return atom.packages.activatePackage("language-todotxt").then(package => {
-        provider = package.mainModule.getAutocompleteProvider();
-      });
-    });
-  });
+        provider = package.mainModule.getAutocompleteProvider()
+      })
+    })
+  })
 
-  it("finds all suggestions with no prefix", () => {
-    let te = atom.workspace.getActiveTextEditor();
-    te.moveDown(9);
-    let text = getCurrentLine(te);
-    expect(text).toEqual("");
-    expect(suggestionsForPrefix(provider, te, "")).toEqual([
+  it("finds all suggestions when activated manually with no prefix", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(9)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("")
+    expect(suggestionsForPrefix(provider, te, { activatedManually: true })).toEqual([
       "@computer",
       "+GarageSale",
       "@GroceryStore",
       "+LanguageTodotxt",
       "@phone"
-    ]);
-  });
+    ])
+  })
 
-});
+  it("finds no suggestions with no prefix", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(9)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("")
+    expect(suggestionsForPrefix(provider, te, { activatedManually: false })).toEqual([])
+  })
+
+  it("labels suggestions correctly", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(9)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("")
+    let suggestions = suggestionsForPrefix(provider, te, { activatedManually: true, raw: true })
+    expect(suggestions[0].text).toEqual("@computer")
+    expect(suggestions[0].type).toEqual("tag")
+    expect(suggestions[0].rightLabel).toEqual("Context")
+
+    expect(suggestions[1].text).toEqual("+GarageSale")
+    expect(suggestions[1].type).toEqual("tag")
+    expect(suggestions[1].rightLabel).toEqual("Project")
+
+    expect(suggestions[2].text).toEqual("@GroceryStore")
+    expect(suggestions[2].type).toEqual("tag")
+    expect(suggestions[2].rightLabel).toEqual("Context")
+
+    expect(suggestions[3].text).toEqual("+LanguageTodotxt")
+    expect(suggestions[3].type).toEqual("tag")
+    expect(suggestions[3].rightLabel).toEqual("Project")
+
+    expect(suggestions[4].text).toEqual("@phone")
+    expect(suggestions[4].type).toEqual("tag")
+    expect(suggestions[4].rightLabel).toEqual("Context")
+  })
+
+  it("finds all projects with + prefix", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(9)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("")
+    te.insertText("+")
+    expect(suggestionsForPrefix(provider, te)).toEqual([
+      "+GarageSale",
+      "+LanguageTodotxt"
+    ])
+  })
+
+  it("finds all contexts with @ prefix", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(9)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("")
+    te.insertText("@")
+    expect(suggestionsForPrefix(provider, te)).toEqual([
+      "@computer",
+      "@GroceryStore",
+      "@phone"
+    ])
+  })
+
+  it("finds only matching contexts with @G prefix", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(9)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("")
+    te.insertText("@G")
+    expect(suggestionsForPrefix(provider, te)).toEqual([
+      "@GroceryStore"
+    ])
+  })
+
+  it("finds only matching projects with +La prefix", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(9)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("")
+    te.insertText("+La")
+    expect(suggestionsForPrefix(provider, te)).toEqual([
+      "+LanguageTodotxt"
+    ])
+  })
+
+  it("finds does not suggest tags that are already on the current line", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(1)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("(B) Schedule Goodwill pickup +GarageSale @phone")
+    te.moveToEndOfLine()
+    te.insertText(" ")
+    expect(suggestionsForPrefix(provider, te, { activatedManually: true })).toEqual([
+      "@computer",
+      "@GroceryStore",
+      "+LanguageTodotxt"
+    ])
+  })
+
+  it("finds does not suggest tags that are already on the current line", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    te.moveDown(1)
+    let text = getCurrentLine(te)
+    expect(text).toEqual("(B) Schedule Goodwill pickup +GarageSale @phone")
+    te.moveToEndOfLine()
+    te.insertText(" ")
+    expect(suggestionsForPrefix(provider, te, { activatedManually: true })).toEqual([
+      "@computer",
+      "@GroceryStore",
+      "+LanguageTodotxt"
+    ])
+  })
+
+  it("finds does not filter out non-tags on the current line", () => {
+    let te = atom.workspace.getActiveTextEditor()
+    let text = getCurrentLine(te)
+    expect(text).toEqual("(A) Thank Mom for the meatballs @phone")
+    te.moveToEndOfLine()
+    te.insertText(" @example.com")
+    te.moveDown(4)
+    text = getCurrentLine(te)
+    expect(text).toEqual("(C) email user@example.com @computer")
+    te.moveToEndOfLine()
+    te.insertText(" @e")
+    expect(suggestionsForPrefix(provider, te)).toEqual([
+      "@example.com"
+    ])
+  })
+})
