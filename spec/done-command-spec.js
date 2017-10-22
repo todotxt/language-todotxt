0ca0eb3794b7d313cd14e85ed5b07a68017f97d9
Matthew Crumley <email@matthewcrumley.com>
2017-10-22 19:40:17 -0400

    Fix test

--- spec/done-command-spec.js
+++ spec/done-command-spec.js
@@ -136,7 +136,8 @@ describe("todo.txt done command", () => {
     let now = `${dt.getFullYear()}-${(dt.getMonth() < 8) ? '0' : ''}${dt.getMonth() + 1}-${dt.getDate() < 10 ? '0' : ''}${dt.getDate()}`;
     expect(text).toEqual(`x ${now} 2017-10-17 Implement the todotxt:done command +LanguageTodotxt pri:A`);
 
-    atom.commands.dispatch(workspaceElement(), 'core:undo');
+    te.undo()
+    text = getCurrentLine(te);
     expect(text).toEqual("(A) 2017-10-17 Implement the todotxt:done command +LanguageTodotxt");
   });
 
